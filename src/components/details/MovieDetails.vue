92112715d3e878f050462349e551f24519f17558
Noud Goedemondt <noudgoedemondt@gmail.com>
2024-10-16 20:16:23 +0200

    add cool layout

--- src/components/details/MovieDetails.vue
+++ src/components/details/MovieDetails.vue
@@ -1,35 +1,53 @@
 <template>
   <v-container v-if="loading" class="text-center">Loading...</v-container>
-  <v-container v-else class="w-720px mt-5 m-auto border">
-    <v-row>
-      <v-col cols="3">
-        <v-img :src="posterUrl" alt="Movie Poster" class="rounded-lg" />
-        <v-sheet class="d-flex align-center justify-center">
-          <span class="mr-1">{{ rating.toFixed(1) }}</span>
-          <v-rating
-            v-model="starRating"
-            length="5"
-            color="amber"
-            density="compact"
-            half-increments
-            readonly
-          ></v-rating>
-          <span class="ml-1">({{ ratingCount }})</span>
-        </v-sheet>
-      </v-col>
-      <v-spacer></v-spacer>
-      <v-col cols="7">
-        <h1 class="font-weight-bold">{{ title }}</h1>
-        <v-chip color="primary" dark>{{ releaseDate }}</v-chip>
-        <p class="my-4">{{ overview }}</p>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div v-else>
+    <v-sheet
+      :style="{
+        backgroundImage: `url(${backdropUrl})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '400px',
+      }"
+    >
+    </v-sheet>
+    <v-container
+      :style="{
+        transform: `translateY(-${overlayAmount})`,
+      }"
+    >
+      <v-row>
+        <v-spacer></v-spacer>
+        <v-col cols="2">
+          <v-img :src="posterUrl" alt="Movie Poster" class="rounded-lg" />
+          <v-sheet class="mt-5 d-flex align-center justify-center">
+            <span class="mr-1">{{ rating.toFixed(1) }}</span>
+            <v-rating
+              v-model="starRating"
+              length="5"
+              color="amber"
+              density="compact"
+              half-increments
+              readonly
+            ></v-rating>
+            <span class="ml-1">({{ ratingCount }})</span>
+          </v-sheet>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col cols="7" :style="`margin-top: ${overlayAmount}`">
+          <h1 class="font-weight-bold">{{ title }}</h1>
+          <v-chip color="primary" dark>{{ releaseDate }}</v-chip>
+          <p class="my-4">{{ overview }}</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script setup>
 import { onMounted, ref, defineProps, computed } from 'vue';
 import { fetchDetails, constructImageUrl } from '@/api/tmdb';
+
+const overlayAmount = '150px';
 
 const props = defineProps(['id']);
 
@@ -50,9 +68,9 @@ const releaseDate = computed(() =>
 const posterUrl = computed(() =>
   data.value ? constructImageUrl('w780', data.value.poster_path) : ''
 );
-// const backdropUrl = computed(() =>
-//   data.value ? constructImageUrl('w1280', data.value.backdrop_path) : ''
-// );
+const backdropUrl = computed(() =>
+  data.value ? constructImageUrl('original', data.value.backdrop_path) : ''
+);
 
 onMounted(async () => {
   try {
