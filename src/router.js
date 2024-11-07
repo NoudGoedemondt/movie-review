30b87bc7b147fdc74f53143ee87748f023c8c904
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-11-07 10:31:54 +0100

    add testing page

--- src/router.js
+++ src/router.js
@@ -2,9 +2,11 @@ import { createWebHistory, createRouter } from 'vue-router';
 
 import HomeView from './views/HomeView.vue';
 import DetailsView from './views/DetailsView.vue';
+import TestView from './views/TestView.vue';
 
 const routes = [
   { path: '/', component: HomeView },
+  { path: '/test', component: TestView },
   { path: '/details/:type/:id', component: DetailsView },
 ];
 
---
+++ src/views/TestView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="container">
+      <details-video :id="'912649'" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import DetailsVideo from '@/components/details/DetailsVideo.vue';
+</script>
+
+<style scoped>
+.container {
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+</style>
